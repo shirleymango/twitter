8bbbc0b6fbb49374d02ec03fc5b27ef6da9ae0ab
Shirley Zhu <shirleyzzhu@fb.com>
2022-06-22 21:56:33 -0700

    created new controller and edited storyboard

---
+++ twitter/DetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.h
+//  twitter
+//
+//  Created by Shirley Zhu on 6/22/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
